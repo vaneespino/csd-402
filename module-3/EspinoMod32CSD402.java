07cf1dcf8dfdb5f6d78247bbcc1877766ee1aad0
Vanessa Espino <vaneespino16@gmail.com>
2026-04-15 22:24:06 -0400

    Add files via upload

---
+++ module-3/EspinoMod32CSD402.java
@@ -0,0 +1,21 @@
+ //Vanessa Espino Module 3.2 Had to write
+
+
+
+public class EspinoMod32CSD402 {
+
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 7; i++) {
+            
+            
+            for (int j = 0; j <= i; j++) {
+                System.out.print((int) Math.pow(2, j) + " ");
+            }
+            
+            
+            System.out.println("@ ");
+            }
+        }
+    }
+
